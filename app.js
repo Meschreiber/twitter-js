7a2e751008bb2e864435110ef5135ac6d21a7b50
Maria Schreiber <meschreiber3@gmail.com>
2017-03-06 18:16:16 -0500

    start script

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+const chalk = require('chalk');
 
 app.listen(3000, function(){
     console.log('server listening');
 });
 
-app.get('/', function(req, res){
-    res.send('Welcome to express!');
+app.use(function (req, res, next) {
+    console.log(chalk.red(req.method) + ' ' + chalk.blue(req.route));
+    next();
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
 })
+
+app.get('/', function(req, res){
+    res.send('Welcome to express!\n');
+})
+
+
