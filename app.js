2894ae90ccf5ada0ee3aea9808445b195c926968
TinaHeiligers <tina.heiligers@gmail.com>
2017-03-07 10:45:36 -0500

    used nunjucks

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 const chalk = require('chalk');
+const nunjucks = require('nunjucks');
+
+var locals = {
+  title: 'Nunjucks Example',
+  people: [
+    { name: 'Gandalf'},
+    { name: 'Frodo' },
+    { name: 'Legolas'}
+  ]
+};
+
+nunjucks.configure('views', {noCache: true});
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
 
 app.listen(3000, function(){
     console.log('server listening');
@@ -9,11 +23,10 @@ app.listen(3000, function(){
 app.use(function (req, res, next) {
     console.log(chalk.red(req.method) + ' ' + chalk.blue(req.route));
     next();
-    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
 })
 
 app.get('/', function(req, res){
-    res.send('Welcome to express!\n');
+  res.render('index', locals)
 })
 
 
