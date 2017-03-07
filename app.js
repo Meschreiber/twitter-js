2249bee7b7550e046cf24e288ab0444bee7196bf
TinaHeiligers <tina.heiligers@gmail.com>
2017-03-07 12:18:31 -0500

    stylesheet router not working

--- app.js
+++ app.js
@@ -17,15 +17,16 @@ nunjucks.configure('views', {noCache: true});
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 
-app.use(function (req, res, next) {
-    console.log(chalk.red(req.method) + ' ' + chalk.blue(req.route));
-    next();
-})
 
 app.use('/', routes);
 // app.get('/', function(req, res){
 //   res.render('index', locals)
 // })
+
+app.use(function (req, res, next) {
+    console.log(chalk.red(req.method) + ' ' + chalk.blue(req.route));
+    next();
+})
 
 app.listen(3000, function(){
     console.log('server listening');
